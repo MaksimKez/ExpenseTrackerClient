cc42d6c1f26e67dfae9b592b87b234b1410b28b0
KezMaksim <prostomaxim765@gmail.com>
2024-11-15 20:05:43 +0100

    very hard problem)

--- ExpenseTrackerClient/LoginWindow.xaml.cs
+++ ExpenseTrackerClient/LoginWindow.xaml.cs
@@ -1,36 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace ExpenseTrackerClient
-{
-    /// <summary>
-    /// Interaction logic for LoginWindow.xaml
-    /// </summary>
-    public partial class LoginWindow : Window
-    {
-        public LoginWindow()
-        {
-            InitializeComponent();
-        }
-        using ExpenseTrackerClient.Data.Models;
-using System;
-using System.Net.Http;
+﻿using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Navigation;
+using ExpenseTrackerClient.Data.Models.Dtos;
 
 namespace ExpenseTrackerClient
     {
