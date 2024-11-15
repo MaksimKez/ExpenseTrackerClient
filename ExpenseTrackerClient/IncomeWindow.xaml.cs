07099409721120a2629a6f8376df4424a344bcd7
KezMaksim <prostomaxim765@gmail.com>
2024-11-15 20:03:56 +0100

    fixed

--- ExpenseTrackerClient/IncomeWindow.xaml.cs
+++ ExpenseTrackerClient/IncomeWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Transactions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ExpenseTrackerClient.Data.HttpClients;
 
 namespace ExpenseTrackerClient
 {
@@ -20,11 +22,20 @@ namespace ExpenseTrackerClient
     /// </summary>
     public partial class IncomeWindow : Window
     {
+        // делаешь клиент который я написал полем
+        public TransactionsClient Client { get; set; }
         public IncomeWindow()
         {
+            // объявляешь
+            Client = new TransactionsClient();
+            
+            
             InitializeComponent();
         }
-        private void Button_Click_Exid(object sender, RoutedEventArgs e)
+        
+        //и используешь его так во всех окнах
+        //todo ВАЖНО, посмотри сами классы, какие они параметры просят и что они возвращают
+        private void Button_Click_Exit(object sender, RoutedEventArgs e)
         {
             NavigationService navigationService = NavigationService.GetNavigationService(this);
             navigationService?.Navigate(new Uri("MainWindow.xaml", UriKind.Relative));
@@ -34,7 +45,7 @@ namespace ExpenseTrackerClient
             NavigationService navigationService = NavigationService.GetNavigationService(this);
             navigationService?.Navigate(new Uri("IncomeWindow.xaml", UriKind.Relative));
         }
-        private void Button_Click_Epense(object sender, RoutedEventArgs e)
+        private void Button_Click_Expense(object sender, RoutedEventArgs e)
         {
             NavigationService navigationService = NavigationService.GetNavigationService(this);
             navigationService?.Navigate(new Uri("ExpenseWindow.xaml", UriKind.Relative));
