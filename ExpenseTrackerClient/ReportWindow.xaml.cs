a7b150e306317b94e3edc99d609793856c86a2a7
Antonina765 <antoninakapuza0@gmail.com>
2024-12-07 17:52:09 -0800

    fix ptoblem in report

--- ExpenseTrackerClient/ReportWindow.xaml.cs
+++ ExpenseTrackerClient/ReportWindow.xaml.cs
@@ -4,8 +4,16 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using ExpenseTrackerClient.Data.HttpClients;
+using ExpenseTrackerClient.Data.Models;
+using ExpenseTrackerClient.Models;
+using Microsoft.Win32;
 using Newtonsoft.Json;
-using OfficeOpenXml; // Не забудьте установить пакет EPPlus для работы с Excel
+using OfficeOpenXml;
+using System.Collections.Generic;
+using DocumentFormat.OpenXml.Packaging; 
+using DocumentFormat.OpenXml.Spreadsheet;
+using OfficeOpenXml.Core.ExcelPackage; // Не забудьте установить пакет EPPlus для работы с Excel
 
 namespace ExpenseTrackerClient;
 
@@ -64,51 +72,57 @@ public partial class ReportWindow : System.Windows.Window
     } 
     private void GenerateReport()
     { 
-        using (var package = new ExcelPackage())
-        { 
-            var worksheet = package.Workbook.Worksheets.Add("Доходы и Расходы"); 
-            
-            // Заголовки
-            worksheet.Cells[1, 1].Value = "Категория";
-            worksheet.Cells[1, 2].Value = "Сумма";
-            worksheet.Cells[1, 3].Value = "Дата";
-            worksheet.Cells[1, 4].Value = "Тип"; 
-            worksheet.Cells[1, 5].Value = "Комментарий";
+        var saveFileDialog = new SaveFileDialog
+        {
+            Filter = "Excel files (*.xlsx)|*.xlsx",
+            FileName = "Отчет"
+        };
 
-            // Данные доходов
-            int row = 1;
-            foreach (var income in _incomes)
-            {
-                worksheet.Cells[row, 1].Value = "Доход";
-                worksheet.Cells[row, 2].Value = income.Sum;
-                worksheet.Cells[row, 3].Value = income.Date;
-                worksheet.Cells[row, 4].Value = income.IncomeSource;
-                worksheet.Cells[row, 5].Value = income.Title; 
-                row++;
-            }
+        if (saveFileDialog.ShowDialog() == true)
+        {
+            var file = new FileInfo(saveFileDialog.FileName);
 
-            // Данные расходов
-            foreach (var expense in _expenses)
-            {
-                worksheet.Cells[row, 1].Value = "Расход";
-                worksheet.Cells[row, 2].Value = expense.Sum;
-                worksheet.Cells[row, 3].Value = expense.Date;
-                worksheet.Cells[row, 4].Value = expense.ExpenseSource;
-                worksheet.Cells[row, 5].Value = expense.Title; 
-                row++;
-            }
+            using (var package = new ExcelPackage(file))
+            { 
+                var worksheet = package.Workbook.Worksheets.Add("Доходы и Расходы"); 
+                
+                worksheet.Cells[1, 1].Value = "Категория";
+                worksheet.Cells[1, 2].Value = "Сумма";
+                worksheet.Cells[1, 3].Value = "Дата";
+                worksheet.Cells[1, 4].Value = "Тип"; 
+                worksheet.Cells[1, 5].Value = "Комментарий"; //Install-Package EPPlus
 
-            var saveFileDialog = new Microsoft.Win32.SaveFileDialog
-            {
-                Filter = "Excel files (*.xlsx)|*.xlsx",
-                FilterIndex = 2,
-                FileName = "Отчет"
-            };
+                int row = 2; // Start from the second row
 
-            if (saveFileDialog.ShowDialog() == true)
-            {
-                var file = new FileInfo(saveFileDialog.FileName);
-                package.SaveAs(file);
+                foreach (var income in _incomes)
+                {
+                    worksheet.Cells[row, 1].Value = "Доход";
+                    worksheet.Cells[row, 2].Value = income.Sum;
+                    worksheet.Cells[row, 3].Value = income.Date;
+                    worksheet.Cells[row, 4].Value = income.IncomeSource;
+                    worksheet.Cells[row, 5].Value = income.Title; 
+                    row++;
+                }
+
+                foreach (var expense in _expenses)
+                {
+                    worksheet.Cells[row, 1].Value = "Расход";
+                    worksheet.Cells[row, 2].Value = expense.Sum;
+                    worksheet.Cells[row, 3].Value = expense.Date;
+                    worksheet.Cells[row, 4].Value = expense.ExpenseSource;
+                    worksheet.Cells[row, 5].Value = expense.Title; 
+                    row++;
+                }
+
+                try
+                {
+                    package.Save();
+                    MessageBox.Show("File saved successfully!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error saving file: {ex.Message}");
+                }
             }
         }
     }
