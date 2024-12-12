4464b91d054593a43287036b534c814f1e95b59f
Antonina765 <antoninakapuza0@gmail.com>
2024-12-12 00:53:00 -0800

    change search and sort window

--- ExpenseTrackerClient/SearchResultsWindow.xaml.cs
+++ ExpenseTrackerClient/SearchResultsWindow.xaml.cs
@@ -1,11 +1,14 @@
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
+using ExpenseTrackerClient.Data.Models;
 
 namespace ExpenseTrackerClient;
 
 public partial class SearchResultsWindow : System.Windows.Window
 { 
+    private List<Income> _incomes = new List<Income>();
+    private List<Expense> _expenses = new List<Expense>();
     public SearchResultsWindow()
     {
         InitializeComponent();
